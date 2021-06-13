62ed92a6a00fc511b275ccbef8607de919286d45
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-06-13 16:00:00 -0300

    Starting repo

---
+++ src/test/java/com/example/spring5petclinic/Spring5PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring5petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

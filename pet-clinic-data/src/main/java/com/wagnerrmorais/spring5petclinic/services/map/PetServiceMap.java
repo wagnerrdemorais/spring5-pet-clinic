27a9f11b7fbda47860bba6137d61aa30b7db9b1c
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-06-17 19:42:08 -0300

    Impl map based services, closes #6

---
+++ pet-clinic-data/src/main/java/com/wagnerrmorais/spring5petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package com.wagnerrmorais.spring5petclinic.services.map;
+
+import com.wagnerrmorais.spring5petclinic.model.Pet;
+import com.wagnerrmorais.spring5petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}

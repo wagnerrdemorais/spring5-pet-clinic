6edbb5475fbd2406a4dd86028e59a326b24c481f
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-06-19 17:52:10 -0300

    Id auto generation implemented. Closes #23

--- pet-clinic-data/src/main/java/com/wagnerrmorais/spring5petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/wagnerrmorais/spring5petclinic/services/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package com.wagnerrmorais.spring5petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.wagnerrmorais.spring5petclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +16,17 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+
+        if (object == null) {
+            throw new RuntimeException("Object cannot be null");
+        }
+
+        if (object.getId() == null) {
+            object.setId(getNextId());
+        }
+        map.put(object.getId(), object);
+
         return object;
     }
 
@@ -28,5 +36,15 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1L;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+        return nextId;
     }
 }

364a07c3b38a292e718a36b084fe38e2e43beb74
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-06-23 20:10:49 -0300

    Added Specialties Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/wagnerrmorais/spring5petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.wagnerrmorais.spring5petclinic.services;
+
+import com.wagnerrmorais.spring5petclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/wagnerrmorais/spring5petclinic/services/map/SpecialtiesMapService.java
@@ -0,0 +1,36 @@
+package com.wagnerrmorais.spring5petclinic.services.map;
+
+import com.wagnerrmorais.spring5petclinic.model.Specialty;
+import com.wagnerrmorais.spring5petclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtiesMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        super.delete(specialty);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return super.save(specialty);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}

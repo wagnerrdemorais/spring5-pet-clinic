10d34e75c66c9f056aa468e0eab4d9c59c5fcd06
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-06-23 19:43:23 -0300

    Implement PetTypeMapService. Closes #29

---
+++ pet-clinic-data/src/main/java/com/wagnerrmorais/spring5petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.wagnerrmorais.spring5petclinic.services;
+
+import com.wagnerrmorais.spring5petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}

eb11252c5d677233cd1b9597b7c482c22efabf36
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-06-26 18:57:06 -0300

    convert owners to jpa. Closes #36

--- pet-clinic-data/src/main/java/com/wagnerrmorais/spring5petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/wagnerrmorais/spring5petclinic/model/Person.java
@@ -1,8 +1,15 @@
 package com.wagnerrmorais.spring5petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass// this class isn't supposed to go to database, but, classes will extend from it
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
--- pet-clinic-data/src/main/java/com/wagnerrmorais/spring5petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/wagnerrmorais/spring5petclinic/model/PetType.java
@@ -1,7 +1,14 @@
 package com.wagnerrmorais.spring5petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pet_type")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {

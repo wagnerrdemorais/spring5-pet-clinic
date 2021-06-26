6320837e3773077255a6752c97c8a1561d858dd1
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-06-26 19:41:19 -0300

    Spring Data jpa for Vets. Closes #41

--- pet-clinic-data/src/main/java/com/wagnerrmorais/spring5petclinic/services/springdatajpa/PetSDJpaService.java
+++ pet-clinic-data/src/main/java/com/wagnerrmorais/spring5petclinic/services/springdatajpa/PetSDJpaService.java
@@ -3,10 +3,14 @@ package com.wagnerrmorais.spring5petclinic.services.springdatajpa;
 import com.wagnerrmorais.spring5petclinic.model.Pet;
 import com.wagnerrmorais.spring5petclinic.repositories.PetRepository;
 import com.wagnerrmorais.spring5petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("notactiveprofile")
 public class PetSDJpaService implements PetService {
 
     private final PetRepository petRepository;
---
+++ pet-clinic-data/src/main/java/com/wagnerrmorais/spring5petclinic/services/springdatajpa/VetSpecialtySDJpaService.java
@@ -0,0 +1,48 @@
+package com.wagnerrmorais.spring5petclinic.services.springdatajpa;
+
+import com.wagnerrmorais.spring5petclinic.model.Specialty;
+import com.wagnerrmorais.spring5petclinic.repositories.SpecialtyRepository;
+import com.wagnerrmorais.spring5petclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("notactiveprofile")
+public class VetSpecialtySDJpaService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public VetSpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return specialtyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return specialtyRepository.save(specialty);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        specialtyRepository.delete(specialty);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialtyRepository.deleteById(id);
+    }
+}

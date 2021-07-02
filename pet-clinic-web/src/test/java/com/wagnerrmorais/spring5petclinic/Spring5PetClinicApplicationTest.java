5050167db548763a0ccaf5b2e85c6d5460396661
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-07-02 20:23:55 -0300

    Surefire dependency added. Closes #4

---
+++ pet-clinic-web/src/test/java/com/wagnerrmorais/spring5petclinic/Spring5PetClinicApplicationTest.java
@@ -0,0 +1,13 @@
+package com.wagnerrmorais.spring5petclinic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Spring5PetClinicApplicationTest {
+
+    @Test
+    public void simpleTest(){
+    }
+
+}
